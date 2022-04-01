0cee573ae2ae556b28ce2c2240960640f824494b
William Spanfelner <wil-1-am@outlook.com>
2022-04-01 11:24:51 +0300

    Refactor write to file code.

--- main.py
+++ main.py
@@ -15,11 +15,7 @@ movies_list = soup.find_all(name="h3", class_="title")
 movies_descending = [movie.getText().split(maxsplit=1) for movie in movies_list]
 top_100_movies = sorted([[int(number[:-1]), title] for number, title in movies_descending])
 
-print(top_100_movies)
-
-file = open("movies.txt", "w")
-for movie in top_100_movies:
-    file.write(f"{movie[0]}) {movie[1]}\n")
-
-file.close()
+with open("movies.txt", "w") as file:
+    for movie in top_100_movies:
+        file.write(f"{movie[0]}) {movie[1]}\n")
 
